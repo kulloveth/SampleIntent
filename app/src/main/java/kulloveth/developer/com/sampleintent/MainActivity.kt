2064859545d03b3d5803f92c704e75c604395a1a
kulloveth <kulloveth@gmail.com>
2020-01-29 13:08:12 +0100

    set up MainActivity

--- app/src/main/java/kulloveth/developer/com/sampleintent/MainActivity.kt
+++ app/src/main/java/kulloveth/developer/com/sampleintent/MainActivity.kt
@@ -1,12 +1,32 @@
 package kulloveth.developer.com.sampleintent
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val phoneNo : String = phoneEditText.text.toString()
+
+        sendButton.setOnClickListener {
+            val intent = Intent(this,WelcomeActivity::class.java)
+            intent.putExtra(PHONE_NO,phoneNo)
+            startActivity(intent)
+        }
+
+
+
     }
+
+
+companion object{
+    const val PHONE_NO : String = "number_extras"
+}
 }
